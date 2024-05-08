8284ebed3b3a6221cbc936248a4acd7ca62cc0ac
unknown <Sena@MEDAPRCSGFSP047.FormacionSena.RED>
2024-05-08 15:10:16 -0500

    feat: sintaxis-variables

---
+++ 01-sintaxis-variables/index.php
@@ -0,0 +1,52 @@
+<!-- <?php
+
+/* ejercicio 1 */
+
+$nombre_1= "Pericles";
+
+$nombre_2="Merlina";
+
+echo " hola $nombre_1 y $nombre_2, ¿Cómo están? <br> <br> Pagina creada por: <br> <br>  - $nombre_1 <br><br> -$nombre_2   <br><br><br><br><br><br><br><br><br><br>";
+
+?>
+
+<?php
+
+$mi_numero = 8;
+
+$respuesta= $mi_numero;
+
+$respuesta += 2;
+
+$respuesta *= 2;
+
+$respuesta -= 2;
+
+$respuesta /= 2;
+
+$respuesta -= $mi_numero;
+
+echo $respuesta
+
+?>
+
+ -->
+
+<?php
+
+echo 'Ejecutando sintaxis varaibles..';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>saludo.php</title>
+</head>
+<body>
+
+
+</body>
+</html>
---
+++ 02-condicionales/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'Eejcutando condicionales...'
+
+
+?>
