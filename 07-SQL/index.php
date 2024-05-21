3a3843665b00bfd39c9ae95f9c90c0b9c85ecaeb
Santiperezxx <santiperezcatano@gmail.com>
2024-05-21 16:55:09 -0500

    feat: SQL-avance

---
+++ 07-SQL/index.php
@@ -0,0 +1,28 @@
+<?php
+
+//Documentacion: SQL W3school
+
+// 1. SQL Select
+// SELECT Edad FROM `estudiantes`;
+// Selecciona las edades de las tablas de estudiantes
+
+// 2. SQL Select distinct
+// SELECT DISTINCT `Estado civil` FROM profesores;
+// Selecciona una de tres opciones que sean similares y elimina als demás
+
+// 3. SQL Where
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
